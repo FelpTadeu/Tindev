c875e8a95440901a12b1668a9231640394e92e2f
Felipe Tadeu <feltadeu.fti@gmail.com>
2020-10-18 17:50:36 -0300

    Configurando Backcend e Banco de Dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const server = express();
 
+mongoose.connect("mongodb+srv://omnistack-8:zaqHgvbyWIg8w5M0@cluster0.qr88y.mongodb.net/omnistack8?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
